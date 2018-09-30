9dd298f513f19003e161f42fbe6a8b5cadd3ee1e
yogaraja gopal <gyogarajaeee@gmail.com>
2018-09-30 22:26:19 +0100

    30092018

--- resolver.js
+++ resolver.js
@@ -1,3 +1,9 @@
+const jwt = require('jsonwebtoken');
+
+const createToken = (user, secret, expiresIn) => {
+    const {username, password} = user;
+    return jwt.sign({username, email}, secret, {expiresIn})
+}
 exports.resolvers = {
     Query : {
         getAllRecipes: async (root, args, {Recipe}) => {
@@ -15,6 +21,19 @@ exports.resolvers = {
                 username
             }).save();
             return newRecipe;
+        },
+        signupUser: async (root, {username, email, password}, {User}) => {
+            const user = await User.findOne({username});
+            if(user){
+                throw new Error('User Already Exists');
+            }
+            const newUser = await new User({
+                username,
+                email,
+                password
+            }).save();
+
+            return { token: createToken(newUser, process.env.SECRET, '1hr')}
         }
     }
 };
